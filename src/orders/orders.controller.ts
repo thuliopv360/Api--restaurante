ca6b5f2ee9665ceb849c7f4bcdc4b556370e4b23
Thulio Paiva <thuliopv360@gmail.com>
2022-07-08 22:18:10 -0300

    feature: created the order to product

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import { Controller, Get, Post, Param, Body } from '@nestjs/common';
 import { OrdersService } from './orders.service';
 import { CreateOrderDto } from './dto/create-order.dto';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -1,17 +1,77 @@
+import { Prisma } from '@prisma/client';
+import { PrismaService } from './../prisma/prisma.service';
 import { Injectable } from '@nestjs/common';
 import { CreateOrderDto } from './dto/create-order.dto';
 
 @Injectable()
 export class OrdersService {
+  constructor(private readonly prisma: PrismaService) {}
   create(dto: CreateOrderDto) {
-    return 'This action adds a new order';
+    const productsIds = dto.products.map((element) => {
+      return { id: element };
+    });
+    const data: Prisma.OrderCreateInput = {
+      table: {
+        connect: {
+          number: dto.tableNumber,
+        },
+      },
+      user: {
+        connect: {
+          id: dto.userId,
+        },
+      },
+      products: {
+        connect: productsIds,
+      },
+    };
+
+    return this.prisma.order.create({
+      data,
+      select: {
+        id: true,
+        createdAt: true,
+        tableNumber: true,
+        userId: true,
+        products: {
+          select: {
+            name: true,
+          },
+        },
+      },
+    });
   }
 
   findAll() {
-    return `This action returns all orders`;
+    return this.prisma.order.findMany({
+      select: {
+        id: true,
+        createdAt: true,
+        tableNumber: true,
+        userId: true,
+        products: {
+          select: {
+            name: true,
+          },
+        },
+      },
+    });
   }
 
   findOne(id: string) {
-    return `This action returns a #${id} order`;
+    return this.prisma.order.findUnique({
+      where: { id },
+      select: {
+        id: true,
+        createdAt: true,
+        tableNumber: true,
+        userId: true,
+        products: {
+          select: {
+            name: true,
+          },
+        },
+      },
+    });
   }
 }
